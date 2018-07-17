ecc17a5303eaa653bc58937d014ffcbf27def19d
XIECI <cisnoc.xie@oocl.com>
2018-07-17 20:39:51 +0800

    first implement, don't refactory

--- js/todo.js
+++ js/todo.js
@@ -22,4 +22,46 @@ $(document)
         }
 
         // code to be implemented
+        $("#button").click(function() {
+            var item = '<li id=' + generateUUID() + ' class=""><input name="done-todo" type="checkbox" class="done-todo"> ' + $("input.input-text").val() + ' </li>';
+            if ($("input.input-text").val()) {
+                $("ol").append(item);
+            }
+            $("input.input-text").val("");
+        })
+
+        $("body").on("click", ".done-todo", function() {
+            var currentClass = $(this).parent().attr("class");
+            if (currentClass === "") {
+                $(this).parent().attr("class", "checked");
+            } else {
+                $(this).parent().attr("class", "");
+            }
+        })
+
+        $("#filters a").click(function() {
+            $("#filters a").attr("class", "");
+            $(this).attr("class", "selected");
+            var text = $(this).text();
+            switch(text) {
+                case "ALL":
+                    $("ol li").show();
+                    break;
+                case "Active":
+                    $("ol li").filter(".checked").hide();
+                    $('ol li[class=""]').show();
+                    break;
+                case "Complete":
+                    $("ol li").filter(".checked").show();
+                    $('ol li[class=""]').hide();
+                    break;
+            }
+        })
+
+        $("body").on("click", "ol li", function() {
+            if ($(this).attr("class") === "") {
+                $(this).children("input").attr("contentEditable", false);
+                $(this).attr("contentEditable", true);
+            }
+        })
     });
